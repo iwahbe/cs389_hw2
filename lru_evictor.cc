19bef0a080efca4021a2b241ef22b8eccaf8970a
Ian Wahbe <ian@wahbe.com>
2020-02-28 17:45:18 -0800

    Add LruEvictor. Testing still needed.

--- lru_evictor.cc
+++ lru_evictor.cc
@@ -1,1 +1,76 @@
 #include "lru_evictor.hh"
+
+LruEvictor::~LruEvictor() {}
+
+void LruEvictor::touch_key(const key_type &key)
+{
+  // removes old element from list
+  std::shared_ptr<LruEvictor::ListNode> node;
+  if ((del_in_list(key) = node) == nullptr) {
+    // we didn't find a node
+    // so we create a new one
+    node = std::shared_ptr<LruEvictor::ListNode>(new LruEvictor::ListNode);
+  }
+  // reset node correctly
+  node->next = nullptr;
+  node->prev = tail;
+  tail->next = node;
+  tail = node.get();
+  map.insert_or_assign(key, node);
+}
+
+// LruEvictor::evict: removes leading element from LList, returning it
+//
+// return const key_type: the least recently touched element
+
+const key_type LruEvictor::evict()
+{
+  auto key = head->key;
+  del_in_list(key);
+  map.erase(key);
+  return key;
+}
+
+// LruEvictor::del_in_list: Deletes the node with `key` from the internal linked
+// list. does not delete from the map. O(1)
+//
+// const key_type &key: the key to delete
+//
+// return: the node removed
+
+std::shared_ptr<LruEvictor::ListNode>
+LruEvictor::del_in_list(const key_type &key)
+{
+  auto found = map.find(key);
+  if (found != map.end()) {
+    // to get a shared_ptr instead of just a ptr
+    auto node = found->second;
+    if (node->prev == nullptr && node->next == nullptr) {
+      // only node in list
+      head = nullptr;
+      tail = nullptr;
+      return node;
+    }
+    else if (node->prev == nullptr) {
+      // node at head of the list
+      node->next->prev = nullptr;
+      head = node->next;
+      return node;
+    }
+    else if (node->next == nullptr) {
+      // node at tail of the list
+      tail = node->prev;
+      tail->next = nullptr;
+      return node;
+    }
+    else {
+      // node in center of the list
+      auto prev = node->prev;
+      auto next = node->next;
+      prev->next = next;
+      next->prev = prev;
+      return node;
+    }
+  }
+  return nullptr;
+}
--- lru_evictor.hh
+++ lru_evictor.hh
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "evictor.hh"
+#include <unordered_map>
 
 class LruEvictor : public Evictor {
     public:
@@ -10,5 +11,18 @@ class LruEvictor : public Evictor {
   const key_type evict() override;
 
     protected:
-  // unknown
+  // Theory: We want the constant time inserts and
+  // deletes of a linked list, but also the constant
+  // time access of a map. We combine these, storing
+  // the most recently used keys in a linked list, and
+  // indexing into the list with a map.
+  struct ListNode {
+    key_type key;
+    std::shared_ptr<ListNode> next;
+    ListNode *prev;
+  };
+  std::shared_ptr<LruEvictor::ListNode> del_in_list(const key_type &key);
+  std::unordered_map<key_type, std::shared_ptr<ListNode>> map;
+  std::shared_ptr<ListNode> head;
+  ListNode *tail;
 };
